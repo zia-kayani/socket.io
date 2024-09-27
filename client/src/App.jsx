589b8ae52cc40afed72c853e8060ffe8b2cde101
Zia <ziakayani.official@gmail.com>
2024-09-27 03:21:14 -0700

    connect front end with backend using socket

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import {io} from 'socket.io-client'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
+  const socket = io('http://localhost:3000/')
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+        <h1>APP</h1>
+    </div>
   )
 }
-
-export default App
